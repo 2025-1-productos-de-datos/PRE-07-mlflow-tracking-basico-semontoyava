428c06f823b6e51d56ef0521546783be25ad454f
semontoyava <semontoyava@unal.edu.co>
2025-05-26 17:51:25 -0500

    mover make_predictions.py

--- make_predictions.py
+++ make_predictions.py
@@ -16,7 +16,7 @@ df = pd.read_csv(FILE_PATH)
 y = df["quality"]
 x = df.drop(columns=["quality"])
 
-## Debe verificarse el run_id del modelo que se quiere cargar
+## Debe verificarse el run_id del modelo que se quiere cargars
 ## Se puede obtener el run_id desde la interfaz de MLflow
 logged_model = "runs:/9d7ef75ea5ba4ad38f0beb59d2a7a7dd/model"
 loaded_model = mlflow.pyfunc.load_model(logged_model)
